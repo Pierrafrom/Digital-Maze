4c45c702548c2bdbb8968146bc170b3a5378078d
azeur365 <49315476+azeur365@users.noreply.github.com>
2024-04-22 01:01:12 +0200

    fix monstre + nv mouvemnts + excla + calcul popup

---
+++ Assets/PixelArtTopDown-Basic/Script/CalculationPopUp.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CalculationPopUp : MonoBehaviour
+{
+    public bool IsPopUpActive { get; private set; }
+
+    public TMP_Text questionText;
+    public TMP_InputField answerInput;
+    public Button submitButton;
+    private int correctAnswer;
+
+    void Start()
+    {
+        submitButton.onClick.AddListener(CheckAnswer);
+        gameObject.SetActive(false);
+    }
+
+    public void GenerateCalculation()
+    {
+        int a = Random.Range(1, 10);
+        int b = Random.Range(1, 10);
+        int operation = Random.Range(0, 4);
+
+        switch (operation)
+        {
+            case 0: //Addition
+                correctAnswer = a + b;
+                questionText.text = $"Solve: {a} + {b} = ?";
+                break;
+            case 1: //Subtraction
+                correctAnswer = a - b;
+                questionText.text = $"Solve: {a} - {b} = ?";
+                break;
+            case 2: //Multiplication
+                correctAnswer = a * b;
+                questionText.text = $"Solve: {a} × {b} = ?";
+                break;
+            case 3: //Division
+                b = b == 0 ? 1 : b;
+                a = a * b;
+                correctAnswer = a / b;
+                questionText.text = $"Solve: {a} ÷ {b} = ?";
+                break;
+        }
+
+        IsPopUpActive = true;
+
+        answerInput.text = "";
+        gameObject.SetActive(true);
+    }
+
+    public void CheckAnswer()
+    {
+
+        if (int.TryParse(answerInput.text, out int userAnswer) && userAnswer == correctAnswer)
+        {
+            IsPopUpActive = false;
+            gameObject.SetActive(false);
+
+            //correct answer
+        }
+        else
+        {
+            //wrong answer
+        }
+    }
+}
